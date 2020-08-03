d8f7f62dfcc629e9c320d6bb8c658de4ee3ba740
Erica <edkjones@gmail.com>
2020-08-03 19:10:31 -0400

    assignment-12-dom

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 function onReady(){
   const addToDoForm = document.getElementById('addToDoForm');
+  const addToDoText = document.getElementById('newToDoText');
+  const addToDoList = document.getElementById('toDoList');
+
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
     //get the text
@@ -10,19 +13,25 @@ function onReady(){
     let checkbox = document.createElement('input');
     //set the input's type to checkbox
     checkbox.type = "checkbox";
+    //delete button
+    let deleteBtn = document.createElement('button');
+    deleteBtn.textContent = "Delete";
+    deleteBtn.addEventListener('click', function(event){
+
+    toDoList.removeChild(this.parentElement);
+  })
     //set the title
     newLi.textContent = title;
     //attach the checkbox to the li
     newLi.appendChild(checkbox);
+    //delete button
+    newLi.appendChild(deleteBtn);
     //attach the li to the ul
     toDoList.appendChild(newLi);
     //empty the input
-    newToDoText.value = '';  
+    newToDoText.value = '';
   });
-}
-  const addToDoText = document.getElementById('newToDoText');
-  const addToDoList = document.getElementById('toDoList');
-}
+};
 window.onload = function () {
   alert("The window has loaded!");
   onReady();
