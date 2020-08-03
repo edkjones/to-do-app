725582b0bd3d273438b7b70b183803c6013fcffd
Erica <edkjones@gmail.com>
2020-08-03 17:32:11 -0400

    Checkpoint 12 Dom Scripting: completed

---
+++ app.js
@@ -0,0 +1,29 @@
+function onReady(){
+  const addToDoForm = document.getElementById('addToDoForm');
+  addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+    //get the text
+    let title = newToDoText.value;
+    //create new li
+    let newLi = document.createElement('li');
+    //create new input
+    let checkbox = document.createElement('input');
+    //set the input's type to checkbox
+    checkbox.type = "checkbox";
+    //set the title
+    newLi.textContent = title;
+    //attach the checkbox to the li
+    newLi.appendChild(checkbox);
+    //attach the li to the ul
+    toDoList.appendChild(newLi);
+    //empty the input
+    newToDoText.value = '';  
+  });
+}
+  const addToDoText = document.getElementById('newToDoText');
+  const addToDoList = document.getElementById('toDoList');
+}
+window.onload = function () {
+  alert("The window has loaded!");
+  onReady();
+};
