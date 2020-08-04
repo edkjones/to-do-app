775a05aca760b1950d8679fd6d1a3e98d21c3838
Erica <edkjones@gmail.com>
2020-08-03 20:03:21 -0400

    check point 13 done

--- app.js
+++ app.js
@@ -1,37 +1,40 @@
 function onReady(){
+  let toDos = [];
   const addToDoForm = document.getElementById('addToDoForm');
-  const addToDoText = document.getElementById('newToDoText');
-  const addToDoList = document.getElementById('toDoList');
+
+  function createNewToDo() {
+    const newToDoText = document.getElementById('newToDoText');
+    if (!newToDoText.value) {return;}
+    toDos.push( {
+      title: newToDoText.value,
+      complete: false,
+
+    });
+    newToDoText.value = '';
+    renderTheUI();
+  }
+function renderTheUI() {
+  const toDoList = document.getElementById('toDoList');
+
+  toDoList.textContent = '';
+
+  toDos.forEach(function(toDo) {
+    const newLi = document.createElement('li');
+    const checkbox = document.createElement('input');
+    checkbox.type = "checkbox";
+
+    newLi.textContent = toDo.title;
+    toDoList.appendChild(newLi);
+    newLi.appendChild(checkbox);
+});
 
   addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
-    //get the text
-    let title = newToDoText.value;
-    //create new li
-    let newLi = document.createElement('li');
-    //create new input
-    let checkbox = document.createElement('input');
-    //set the input's type to checkbox
-    checkbox.type = "checkbox";
-    //delete button
-    let deleteBtn = document.createElement('button');
-    deleteBtn.textContent = "Delete";
-    deleteBtn.addEventListener('click', function(event){
-
-    toDoList.removeChild(this.parentElement);
-  })
-    //set the title
-    newLi.textContent = title;
-    //attach the checkbox to the li
-    newLi.appendChild(checkbox);
-    //delete button
-    newLi.appendChild(deleteBtn);
-    //attach the li to the ul
-    toDoList.appendChild(newLi);
-    //empty the input
-    newToDoText.value = '';
+    createNewToDo();
   });
-};
+renderTheUI();
+}
+
 window.onload = function () {
   alert("The window has loaded!");
   onReady();
